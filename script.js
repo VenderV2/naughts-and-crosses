441120094c1ad4695b66a543b98daaa02bd8bb5b
Nikil <nik300601@gmail.com>
2024-01-10 15:25:43 +0000

    completed game logic other than win conditions

--- script.js
+++ script.js
@@ -10,17 +10,24 @@ const computer = new Player('O');
 
 class Gameboard {
     constructor() {
-        const grid1  = '';
-        const grid2  = '';
-        const grid3  = '';
-        const grid4  = '';
-        const grid5  = '';
-        const grid6  = '';
-        const grid7  = '';
-        const grid8  = '';
-        const grid9  = '';
+        this.grid1  = '';
+        this.grid2  = '';
+        this.grid3  = '';
+        this.grid4  = '';
+        this.grid5  = '';
+        this.grid6  = '';
+        this.grid7  = '';
+        this.grid8  = '';
+        this.grid9  = '';
+
+        this.gridLayout = 
+        [[this.grid1,this.grid2,this.grid3],
+        [this.grid4,this.grid5,this.grid6],
+        [this.grid7,this.grid8,this.grid9]]
+
     }
 }
+let startup = new Gameboard()
 
 class GameLogic {
     constructor() {
@@ -36,28 +43,52 @@ class GameLogic {
 }
 
 let gameLogic = new GameLogic()
-console.log(gameLogic.playersTurn)
+// console.log(gameLogic.playersTurn)
+let sign = '';
+
 
 const gameboard = document.querySelector('.gameboard')
 const gridElements = document.querySelectorAll('#grid')
 gridElements.forEach(element => {
     element.addEventListener('click', () => {
-        gameLogic.playersTurn = !gameLogic.playersTurn;
-        console.log(gameLogic.playersTurn)   
+
+        // console.log(gameLogic.playersTurn)   
         if (gameLogic.playersTurn == true) {
             console.log('x')
+            sign = 'x'
+            element.textContent = 'X'
         }
         else if (gameLogic.playersTurn == false) {
             console.log('o')
+            sign = 'o'
+            element.textContent = 'O'
         }
-   
-    }, {capture: true})
+        gameLogic.playersTurn = !gameLogic.playersTurn;
+        // console.log(element.className)
+        activeElement(element.className)
+    })
 
 
 });
 
-function say() {
-    console.log(gameLogic.playersTurn)
+function activeElement(elementID) {
+    startup[elementID] = sign;
+    startup.gridLayout = 
+    [[startup.grid1,startup.grid2,startup.grid3],
+    [startup.grid4,startup.grid5,startup.grid6],
+    [startup.grid7,startup.grid8,startup.grid9]]
+    Update()
 }
 
-
+function Update() {
+    for (let i = 0; i < startup.gridLayout.length ; i++) {
+        console.log(i)
+        if (startup.gridLayout[i].toString() === ['x','x','x'].toString()) {
+            alert('player wins')
+        }
+        
+        for (let j = 0; j < startup.gridLayout.length - 1; j++) {
+        
+        }
+    }
+}
